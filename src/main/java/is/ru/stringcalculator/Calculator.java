05df5663659eeaf327c8f14c683f3dbce95ab975
katrins11 <katrins11@ru.is>
2014-10-16 22:28:45 +0000

    Test for new line as delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,9 +6,12 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if(text.contains(",\n")){
 			return sum(splitNumbers(text));
 		}
+		//else if(test.contains("\n")){
+			//return sum(splitNumbers(text));
+		//}
 		else
 			return 1;
 	}
@@ -18,7 +21,7 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split(",\n");
 	}
       
     private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,10 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    @Test
+    public void testNewLineAsDelimiter(){
+    	assertEquals(6, Calculator.add("1\n,3"));
+    }
+
+
 }
