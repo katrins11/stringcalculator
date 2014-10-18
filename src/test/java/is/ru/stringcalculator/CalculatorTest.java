0d0aba9a0891e80819fcf3142f435a725e0acdfb
Katrin <katrins11@ru.is>
2014-10-18 12:15:20 +0000

    Test for negative numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,42 +5,54 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	public static void main(String args[]) {
+	public static void main(String args[])throws NegativeNumbException {
       		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     	}
 
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString()throws NegativeNumbException{
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber()throws NegativeNumbException{
 		assertEquals(1, Calculator.add("1"));
 	}
 
 	@Test
-	public void testTwoNumbers() {
+	public void testTwoNumbers()throws NegativeNumbException{
 		assertEquals(3, Calculator.add("1,2"));
 	}
 
 	@Test
-   	public void testMultipleNumbers(){
+   	public void testMultipleNumbers()throws NegativeNumbException{
     		assertEquals(6, Calculator.add("1,2,3"));
     	}
 
    	@Test
-    	public void testNewLineAsDelimiter(){
+    	public void testNewLineAsDelimiter()throws NegativeNumbException{
     		assertEquals(6, Calculator.add("1\n2,3"));
     	}
 
 	@Test
-	public void testChangeTheDelimiter(){
+	public void testChangeTheDelimiter()throws NegativeNumbException{
 		assertEquals(3, Calculator.add("//;\n1;2"));
 	}
 
 	@Test
-	public void testNumberBiggerThen1000(){
+	public void testNvegativesNotAllowed()throws NegativeNumbException{
+		try{
+			calculator.add("-1,2");
+			fail();
+		}
+		catch(NegativeNumbException x)
+		{
+			assertEquals("Negatives not allowed: -1", NegativeNumbException.getmessage(x));
+		}
+	}
+
+	@Test
+	public void testNumberBiggerThen1000()throws NegativeNumbException{
 		assertEquals(2, Calculator.add("1001,2"));
 	}
 
